7fc05d7e56d894bb0023d9ae307a69f66b36a6de
tunakdu <akduhant@gmail.com>
2021-07-11 12:30:06 +0300

    binom first

---
+++ partials/foot-scripts.php
@@ -0,0 +1,18 @@
+<!-- JavaScript -->
+
+
+
+
+
+
+
+
+
+
+
+<script src="assets/js/jquery.bundle.js?ver=1930"></script>
+<script src="assets/js/scripts.js?ver=1930"></script>
+<script src="assets/js/charts.js?var=161"></script>
+</body>
+
+</html>
---
+++ partials/headers.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="zxx" class="js">
+<head>
+	<meta charset="utf-8">
+<meta name="author" content="Softnio">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<!-- Fav Icon  -->
+<link rel="shortcut icon" href="images/favicon.png">
+<!-- Site Title  -->
+<title>Salvia | ICO Crypto - ICO Landing Page &amp; Multi-Purpose Cryptocurrency HTML Template</title>
+<!-- Bundle and Base CSS -->
+<link rel="stylesheet" href="assets/css/vendor.bundle.css?ver=1930">
+<link rel="stylesheet" href="assets/css/style-salvia.css?ver=1930" id="changeTheme">
+<!-- Extra CSS -->
+<link rel="stylesheet" href="assets/css/theme.css?ver=1930">
+
+
+
+</head>
+     
